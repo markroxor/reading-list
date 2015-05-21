bbe219f26c1a4814c3aada3dbbd41d44083443d2
Brandon Amos <bamos@cs.cmu.edu>
2015-05-21 13:04:25 -0400

    Move scripts into separate directory

--- scripts/random-book.rb
+++ scripts/random-book.rb
@@ -11,9 +11,9 @@ def prettyBook(b)
   "#{b['author']} - #{b['title']}"
 end
 
-books = YAML.load_file('to-read.yaml')
+books = YAML.load_file('data/to-read.yaml')
 book_list = books.sample(num_choices)
-puts "#{num_choices} random books I could be reading:"
+puts "#{num_choices} random books I could be reading:\n\n"
 (1..num_choices).map { |n|
   puts "#{n}. #{prettyBook(book_list[n-1])}"
 }
