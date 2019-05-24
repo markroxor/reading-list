8f73428a4ec3e6ace200fc09675fb7e2317f6ce9
Mohit Rathore <mrmohitrathoremr@gmail.com>
2019-05-24 16:37:21 +0530

    script

--- scripts/get_notes.py
+++ scripts/get_notes.py
@@ -6,7 +6,12 @@ print('Reading file..')
 text = textract.process(sys.argv[1])
 print('Read file.')
 
-with open(sys.argv[2], 'w') as f:
+with open(sys.argv[2], 'a') as f:
+    f.write('\n- author: Harari, Yuval Noah')
+    f.write('\n  title: A Brief History of Humankind, Sapiens.')
+    f.write('\n  finished: 2019-05-15')
+    f.write('\n  rating: 4.2')
+    f.write('\n  quotes:')
     tex = ''
     p = ''
     for t in re.compile(b'Mohit Rathore').split(text):
@@ -20,7 +25,11 @@ with open(sys.argv[2], 'w') as f:
             continue
 
         page = t[-1]
-        a = "\n- page: " +  "\"" + page + "\"\n"
-        b = "  content: " + ' '.join(tex.replace(':', '').split()[:-2])
-        f.write(a+b)
+        content = ' '.join(tex.replace(':', '').split()[:-2])
+        a = "\n        - page: " +  "\"" + page + "\"\n"
+        b = "          content: " + content
+
+        if len(content) < 100000 and len(content) > 2:
+            f.write(a+b)
+
         tex = ''
