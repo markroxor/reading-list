629411e8de43ac247df0deafe9aae103f1d53c29
Mohit Rathore <mrmohitrathoremr@gmail.com>
2019-08-31 15:08:17 +0530

    art of invisibility

--- scripts/get_notes.py
+++ scripts/get_notes.py
@@ -6,30 +6,26 @@ print('Reading file..')
 text = textract.process(sys.argv[1])
 print('Read file.')
 
-with open(sys.argv[2], 'a') as f:
-    f.write('\n- author: Harari, Yuval Noah')
-    f.write('\n  title: A Brief History of Humankind, Sapiens.')
-    f.write('\n  finished: 2019-05-15')
-    f.write('\n  rating: 4.2')
+with open(sys.argv[2], 'w') as f:
+    f.write('\n- author: Mitnick, Kevin')
+    f.write('\n  title: The Art Of Invisibility.')
+    f.write('\n  finished: 2019-08-31')
+    f.write('\n  rating: 4.8')
     f.write('\n  quotes:')
     tex = ''
     p = ''
     for t in re.compile(b'Mohit Rathore').split(text):
         t = t.split(b'\n')[2:-1]
         t = b' '.join(t).decode('utf8').split()
-        tex = tex + ' ' + " ".join(t)
 
-        if len(t) < 2 or 'Page' not in t[-2]:
-            if len(t) >1:
-                print(t[-2])
+        if 'Page' not in t[0]:
             continue
-
-        page = t[-1]
-        content = ' '.join(tex.replace(':', '').split()[:-2])
+        page = t[1]
+        content = ' '.join(t[4:])
         a = "\n        - page: " +  "\"" + page + "\"\n"
         b = "          content: " + content
 
-        if len(content) < 100000 and len(content) > 2:
-            f.write(a+b)
+        # if len(content) < 100000 and len(content) > 2:
+        f.write(a+b)
 
-        tex = ''
+        # tex = ''
